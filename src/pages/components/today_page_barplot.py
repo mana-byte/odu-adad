5e73467c1d63c2285f899b8315a4763146f066ef
mana-byte <manaikilaut@gmail.com>
2026-02-17 17:20:44 +0100

    structure: add components

---
+++ src/pages/components/today_page_barplot.py
@@ -0,0 +1,51 @@
+from numpy import column_stack
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+from matplotlib.cm import ScalarMappable
+
+def bar_plots_radio_tv():
+    radio_col, tv_col = st.columns(2, gap="large")
+
+    # Load data
+    df_channels = pd.read_csv(
+        "https://www.data.gouv.fr/api/1/datasets/r/756365eb-a8ae-42b1-8345-76fda5dde110"
+    )
+
+    # TV Data
+    with tv_col:
+        tv = df_channels[df_channels["media"] == "tv"].copy()
+        tv.loc[tv["women_expression_rate_2020"].isna(), "women_expression_rate_2020"] = (
+            tv.loc[tv["women_expression_rate_2020"].isna(), "women_expression_rate_2019"]
+        )
+        tv = tv.sort_values("women_expression_rate_2020", ascending=False)
+        tv = tv[tv["Editeur"] != "FRANCE 4"]
+        tv["women_pct"] = tv["women_expression_rate_2020"] * 100
+
+        st.write("### Chaînes TV classées par part de parole féminine (2020)")
+        tv_chart = st.bar_chart(
+            tv.set_index("Editeur")["women_pct"],
+            use_container_width=True,
+            horizontal=True,
+            sort="-women_pct"
+        )
+
+    # Radio Data
+    with radio_col:
+        radio = df_channels[df_channels["media"] == "radio"].copy()
+        radio.loc[
+            radio["women_expression_rate_2020"].isna(), "women_expression_rate_2020"
+        ] = radio.loc[
+            radio["women_expression_rate_2020"].isna(), "women_expression_rate_2019"
+        ]
+        radio = radio.sort_values("women_expression_rate_2020", ascending=False)
+        radio["women_pct"] = radio["women_expression_rate_2020"] * 100
+
+        st.write("### Stations de radio classées par part de parole féminine (2020)")
+        radio_chart = st.bar_chart(
+            radio.set_index("Editeur")["women_pct"],
+            use_container_width=True,
+            horizontal=True,
+            sort="-women_pct"
+        )
---
+++ src/pages/today_page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+from pages.components.today_page_barplot import bar_plots_radio_tv
+
+st.title("Aujourd'hui")
+
+bar_plots_radio_tv()
+
+
