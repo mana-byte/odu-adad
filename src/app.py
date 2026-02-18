87c9123950d37d30642010a313813411a8c853ec
AGachelin <159674898+AGachelin@users.noreply.github.com>
2026-02-18 18:53:56 +0100

    app: custom page names

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
-st.text("dummy")
+pg = st.navigation([st.Page("pages/col1.py", title="Évolution 1995-2019"), st.Page("pages/today_page.py", title="Aujourd'hui"), st.Page("pages/col3.py", title="Impact de la thématique abordée")])
+pg.run()
