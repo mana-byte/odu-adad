86f6f94de13a90ff8f0f31bb0afac279a68195d1
mana-byte <manaikilaut@gmail.com>
2026-03-22 10:11:55 +0100

    nav_bar: add icons

--- src/app.py
+++ src/app.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 pg = st.navigation(
     [
-        st.Page("pages/evolution_page.py"),
-        st.Page("pages/today_page.py"),
+        st.Page("pages/evolution_page.py", title="Évolution 1995-2019", icon="📺"),
+        st.Page("pages/today_page.py", title="Aujourd'hui", icon="📺"),
         st.Page(
-            "pages/impact_page.py",
+            "pages/impact_page.py", title="Impact de la thématique abordée", icon="📺"
         ),
     ]
 )
